c0a8173d1d85a43c59703300faa59833d9d80d5c
Monisha <monishakrishnan0904@gmail.com>
2019-09-17 10:27:11 +0530

    sop added

--- JDBC/src/com/jdbc/JdbcUpdate.java
+++ JDBC/src/com/jdbc/JdbcUpdate.java
@@ -28,7 +28,7 @@ public class JdbcUpdate {
 			preparedStatement = connection.prepareStatement("UPDATE login set status=?");
 			
 			preparedStatement.setString(1, "E");
-			
+			System.out.println("Updates successfully");
 			int rowsEffected = preparedStatement.executeUpdate();
 			
 			if(rowsEffected > 0) {
